6478c3beb9a5fde8116d5452a1ac368ec8b11cc8
DESKTOP-KOQMP2O\Ekapob <ekapob.art@hotmail.com>
2017-08-24 14:39:49 +0700

    no message

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
+<?php header("Cache-Control: max-age=2592000"); ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
+   
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
